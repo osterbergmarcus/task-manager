4f8ac51aa95ddcdf3f207eabd8f52b92748b5ee7
Marcus Osterberg <marcusterberg@gmail.com>
2016-03-25 20:06:04 +0100

    Added routes to separate file

--- client/root.js
+++ client/root.js
@@ -1,24 +1,17 @@
-import React                                      from 'react'
-import { render }                                 from 'react-dom'
-import { Provider }                               from 'react-redux'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
-import Firebase                                   from 'firebase'
-import initialState                               from '../src/redux/initialState'
-import store                                      from '../src/redux/store/store'
-import { App, Featured, AddTasks }                from '../src/containers'
+import React                    from 'react'
+import { render }               from 'react-dom'
+import { Provider }             from 'react-redux'
+import { Router, hashHistory }  from 'react-router'
+import Firebase                 from 'firebase'
+import store                    from '../src/redux/store/store'
+import routes                   from './routes'
 
 //render the main component and mount to the root DOM elements
 //Subscribe and make store available to all nested components using Provider
 //Making all childrens of the app component
 render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-       <IndexRoute component={Featured}></IndexRoute>
-        <Route path="/featured" component={Featured}></Route>
-        <Route path="/addtasks" component={AddTasks}></Route>
-       </Route>
-    </Router>
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
   document.getElementById('react-mount')
 )
