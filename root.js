5efb0773460a4a3d3a185f3c665bbcbfda182a21
Marcus Osterberg <osterbergmarcus@users.noreply.github.com>
2016-04-04 22:36:49 +0200

    Added public files for gh-pages

---
+++ root.js
@@ -0,0 +1,18 @@
+import React                    from 'react'
+import { render }               from 'react-dom'
+import { Provider }             from 'react-redux'
+import { Router, hashHistory }  from 'react-router'
+import Firebase                 from 'firebase'
+import store                    from '../src/redux/store/store'
+import routes                   from '../src/routes'
+
+/** render the main component and mount to the root DOM elements
+*   Subscribe and make store available to all nested components using Provider
+*   Making all childrens of the app component
+*/
+render(
+  <Provider store={store}>
+    <Router history={hashHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('react-mount')
+)
